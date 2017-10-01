ca53228d2582d832317fbe18a4cb3dfc52b8bf52
Francesco Mecca <me@francescomecca.eu>
2017-10-01 02:22:10 +0200

    second minidox layout

--- keyboards/minidox/keymaps/mecca/keymap.c
+++ keyboards/minidox/keymaps/mecca/keymap.c
@@ -20,9 +20,15 @@ extern keymap_config_t keymap_config;
 // Defines CTRL ALT CANC
 #define CALTDEL LCTL(LALT(KC_DEL))
 
+
+/* This makes it easier for fast typists to use dual-function keys. 
+   Without this, if you let go of a held key inside the tapping term, it won't register.
+*/ 
+#define PERMISSIVE_HOLD
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/* Qwerty
+/* _DVORAK
  *
  * ,----------------------------------.           ,----------------------------------.
  * |   Q  |   W  |   E  |   R  |   T  |           |   Y  |   U  |   I  |   O  |   P  |
@@ -32,28 +38,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |   Z  |   X  |   C  |   V  |   B  |           |   N  |   M  |   ,  |   .  |   /  |
  * `----------------------------------'           `----------------------------------'
  *                  ,--------------------.    ,------,-------------.
- *                  | Ctrl | Shift| OTH  |    |SYM   | ENTER| Del  |
- *    Hold key      `-ESC----Space| Bkspc|    |Tab   |------+------.
+ *                  | Ctrl | Shift| OTH  |    |SYM   | Bkspc| Del  |
+ *    Hold key      `-ESC---- Tab | Space|    |Enter |------+------.
  *    for Esc or Space            |      |    |      |
  *    or Bkspc or Tab             `------'    `------'
  */
-[_QWERTY] = KEYMAP( \
+[_DVORAK] = KEYMAP( \
   KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,         KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    \
   KC_A,    KC_O,    KC_E,    KC_U,    KC_I,         KC_D,    KC_H,    KC_T,    KC_N,    KC_S, \
   KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,         KC_B,    KC_M,    KC_W,    KC_V,    KC_Z, \
-  \
- CTL_T(KC_ESC), SFT_T(KC_SPC),LT(_OTHERS, KC_BSPACE),  
- \                                                 LT(_SYMBOLS, KC_TAB), KC_ENT, KC_DEL                 \
+
+ CTL_T(KC_ESC), SFT_T(KC_TAB),LT(_OTHERS, KC_SPACE), \
+                                                   	   LT(_SYMBOLS, KC_ENT), KC_BSPACE, KC_DEL                 \
 ),
 
-/* Raise
+/* _SYMBOLS
  *
  * ,----------------------------------.           ,----------------------------------.
- * |  !   |  @   |  {   |   }  |  - _ |           |   0  |   7  |   8  |   9  |   *  |
+ * |  !   |  @   |  {   |   }  |  - _ |           |  / ?|   7  |   8  |   9  |   *  |
  * |------+------+------+------+------|           |------+------+------+------+------|
  * |   #  |  $   |  (   |   )  |      |           |  \ | |   4  |   5  |   6  |  = + |
  * |------+------+------+------+------|           |------+------+------+------+------|
- * |  %   |  ^   |  [   |  ]   |      |           |      |   1  |   2  |   3  |   / ?|
+ * |  %   |  ^   |  [   |  ]   |      |           |      |   1  |   2  |   3  |  0   |
  * `----------------------------------'           `----------------------------------'
  *                  ,--------------------.    ,------,-------------.
  *                  |      | LOWER|      |    |      | RAISE|      |
@@ -61,14 +67,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                |      |    |      |
  *                                `------'    `------'
  */
-[_RAISE] = KEYMAP( \
+[_SYMBOLS] = KEYMAP( \
   KC_EXLM,  KC_AT,  KC_LCBR, KC_RCBR,   KC_MINS,         KC_0,    KC_7,    KC_8,    KC_9, KC_ASTR,    \
-  KC_HASH,  KC_DLR,   KC_LPRN, KC_RPRN, _______,      KC_BSLS,    KC_4,    KC_5,    KC_4, KC_PLUS, \
+  KC_HASH,  KC_DLR,   KC_LPRN, KC_RPRN, KC_MINS,      KC_BSLS,    KC_4,    KC_5,    KC_4, KC_PLUS, \
   KC_PERC,  KC_CIRC,  KC_LBRC, KC_RBRC, _______,      _______,    KC_1,    KC_2,    KC_3, KC_SLSH, \
                       _______, _______, _______,      _______, _______, _______                    \
 ),
 
-/* Lower
+/* _OTHERS
  *
  * ,----------------------------------.           ,----------------------------------.
  * |  F1  |  F2  |  F3  |  F4  |  F5  |           |  PGUP| HOME |  UP  | END  | PGDN  |
@@ -83,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                |      |    |      |
  *                                `------'    `------'
  */
-[_LOWER] = KEYMAP( \
+[_OTHERS] = KEYMAP( \
   KC_F1,     KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_PGUP, KC_HOME, KC_UP, KC_END,     KC_PGDN, \
   KC_F6,     KC_F7,   KC_F8,   KC_F9,  KC_F10,      _______, KC_LEFT, KC_DOWN, KC_RIGHT, KC_RCBR, \
   KC_F11,   KC_F12, _______, _______, KC_CAPS,      _______, _______, _______, KC_PIPE,  KC_DQT, \
@@ -105,10 +111,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                |      |    |      |
  *                                `------'    `------'
  */
-[_ADJUST] =  KEYMAP( \
+[_UNUSED] =  KEYMAP( \
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,        KC_F6,   KC_F7,   KC_UP,   KC_F9,   KC_F10, \
   KC_F11,  KC_F12,  _______, RGB_SAI, RGB_SAD,      _______, KC_LEFT, KC_DOWN, KC_RGHT, CALTDEL, \
-  RESET,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD,      RGB_VAI, RGB_VAD, KC_F8, TSKMGR,  _______, \
+  RESET,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD,      RGB_VAI, RGB_VAD, KC_F8, _______,  _______, \
                     _______, _______, _______,      _______,  _______, _______                    \
 )
 };
