bcdc202b6aa1e1285d9a22b293881d0c09e81cdf
Francesco Mecca <me@francescomecca.eu>
2017-10-05 22:30:44 +0200

    reduced delay on LT

--- keyboards/minidox/keymaps/mecca/keymap.c
+++ keyboards/minidox/keymaps/mecca/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |   Z  |   X  |   C  |   V  |   B  |           |   N  |   M  |   ,  |   .  |   /  |
  * `----------------------------------'           `----------------------------------'
  *                  ,--------------------.    ,------,-------------.
- *                  | Ctrl | Shift| SYM  |    |OTH   | Bkspc| Del  |
+ *                  | Ctrl | Shift| OTH  |    |SYM   | Bkspc| Del  |
  *    Hold key      `-ESC---- Tab | Space|    |Enter |------+------.
  *    for Esc or Space            |      |    |      |
  *    or Bkspc or Tab             `------'    `------'
@@ -48,16 +48,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_A,    KC_O,    KC_E,    KC_U,    KC_I,         KC_D,    KC_H,    KC_T,    KC_N,    KC_S, \
   KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,         KC_B,    KC_M,    KC_W,    KC_V,    KC_Z, \
 
- CTL_T(KC_ESC), SFT_T(KC_TAB),LT(_OTHERS, KC_SPACE), \
-                                                   	   LT(_SYMBOLS, KC_ENT), KC_BSPACE, ALT_T(KC_DEL)                 \
+ CTL_T(KC_ESC), SFT_T(KC_TAB),LT(_SYMBOLS, KC_SPACE), \
+                                                   	   LT(_OTHERS, KC_ENT), KC_BSPACE, ALT_T(KC_DEL)                 \
 ),
 
 /* _SYMBOLS
  *
  * ,----------------------------------.           ,----------------------------------.
- * |  !   |  @   |  {   |   }  |  - _ |           |  / ?|   7  |   8  |   9  |   *  |
+ * |  !   |  @   |  {   |   }  |   &  |           |  / ?|   7  |   8  |   9  |   *  |
  * |------+------+------+------+------|           |------+------+------+------+------|
- * |   #  |  $   |  (   |   )  |  -   |           |  \ | |   4  |   5  |   6  |  = + |
+ * |   #  |  $   |  (   |   )  |  -	_ |           |  \ | |   4  |   5  |   6  |  = + |
  * |------+------+------+------+------|           |------+------+------+------+------|
  * |  %   |  ^   |  [   |  ]   |      |           |      |   1  |   2  |   3  |  `   |
  * `----------------------------------'           `----------------------------------'
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                `------'    `------'
  */
 [_SYMBOLS] = KEYMAP( \
-  KC_EXLM,  KC_AT,  KC_LCBR, KC_RCBR,   KC_MINS,      KC_SLSH,    KC_7,    KC_8,    KC_9, KC_ASTR,    \
+  KC_EXLM,  KC_AT,  KC_LCBR, KC_RCBR,   KC_AMPR,      KC_SLSH,    KC_7,    KC_8,    KC_9, KC_ASTR,    \
   KC_HASH,  KC_DLR,   KC_LPRN, KC_RPRN, KC_MINS,      KC_BSLS,    KC_4,    KC_5,    KC_6, KC_EQUAL, \
   KC_PERC,  KC_CIRC,  KC_LBRC, KC_RBRC, _______,         KC_0,    KC_1,    KC_2,    KC_3, KC_GRV, \
                       _______, _______, _______,      _______, _______, _______                    \
@@ -116,10 +116,47 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                |      |    |      |
  *                                `------'    `------'
  */
-[_UNUSED] =  KEYMAP( \
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,        KC_F6,   KC_F7,   KC_UP,   KC_F9,   KC_F10, \
-  KC_F11,  KC_F12,  _______, RGB_SAI, RGB_SAD,      _______, KC_LEFT, KC_DOWN, KC_RGHT, CALTDEL, \
-  RESET,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD,      RGB_VAI, RGB_VAD, KC_F8, _______,  _______, \
-                    _______, _______, _______,      _______,  _______, _______                    \
-)
+/*[_UNUSED] =  KEYMAP( \*/
+  /*KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,        KC_F6,   KC_F7,   KC_UP,   KC_F9,   KC_F10, \*/
+  /*KC_F11,  KC_F12,  _______, RGB_SAI, RGB_SAD,      _______, KC_LEFT, KC_DOWN, KC_RGHT, CALTDEL, \*/
+  /*RESET,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD,      RGB_VAI, RGB_VAD, KC_F8, _______,  _______, \*/
+                    /*_______, _______, _______,      _______,  _______, _______                    \*/
+/*)*/
 };
+
+// hook on keypresses functions
+// because sometimes when I press space very fast, LT is not called
+static bool flag = false;
+static bool otherKeyPressed = false;
+static uint16_t time = 0;
+#define LIMIT 350
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record)
+{
+	switch (keycode) {
+		case (KC_SPACE): 
+			flag = true;
+			otherKeyPressed = false;
+			time = record->event.time;
+			layer_on(_SYMBOLS);
+			return false; // as if space was not pressed 
+		default:
+			if (flag){
+				otherKeyPressed = true;
+				// event.pressed is a bool, true for pressed, false for release
+			}
+			return true;
+	}
+}
+
+
+void matrix_scan_user(void) {
+	if (flag && timer_elapsed(time) > LIMIT) {
+		flag = false;
+		layer_off(_SYMBOLS);
+		if (!otherKeyPressed){
+			// make a space appear
+			register_code(KC_SPACE);
+		}
+	}
+}
