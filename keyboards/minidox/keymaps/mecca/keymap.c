49e6ede0587f54a0a0b690688e3a539d4adaa1f0
Francesco Mecca <me@francescomecca.eu>
2018-12-04 17:44:49 +0100

    alt key moved

--- keyboards/minidox/keymaps/mecca/keymap.c
+++ keyboards/minidox/keymaps/mecca/keymap.c
@@ -44,12 +44,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *    or Bkspc or Tab             `------'    `------'
  */
 [_DVORAK] = LAYOUT( \
-  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,         KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    \
+                   KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,         KC_F,    KC_G,    KC_C,    KC_R,    ALT_T(KC_L), \
   KC_A,    KC_O,    KC_E,    KC_U,    KC_I,         KC_D,    KC_H,    KC_T,    KC_N,    KC_S, \
   KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,         KC_B,    KC_M,    KC_W,    KC_V,    KC_Z, \
 
  CTL_T(KC_ESC), SFT_T(KC_TAB),LT(_SYMBOLS, KC_SPACE), \
-                                                   	   LT(_OTHERS, KC_ENT), KC_BSPACE, ALT_T(KC_DEL)                 \
+                                                   	   LT(_OTHERS, KC_ENT), KC_BSPACE, KC_DEL  \
 ),
 
 /* _SYMBOLS
